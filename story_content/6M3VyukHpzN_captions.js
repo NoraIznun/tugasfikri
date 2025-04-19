e9f26b1cd962c51267b36e3f68dddf24bc546369
NoraIznun28 <norahanum28@guru.sma.belajar.id>
2025-04-19 14:55:18 +0700

    file upload

---
+++ story_content/6M3VyukHpzN_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%203.98.34380.0%0D%0A%0D%0A00:00:00.075%20--%3E%2000:00:02.061%0D%0AKevin%20and%20Billy%20have%20eight%20avocados%0D%0A%0D%0A"}')
